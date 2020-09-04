f15e4d83c1ebaeb333a8ec7fb03001fb7934e085
anapao07 <annapao.arcos@gmail.com>
2020-09-03 22:59:56 -0500

    Get Pokemon id

--- pokemonApp/rest.py
+++ pokemonApp/rest.py
@@ -1,4 +1,10 @@
 import requests
+import re
+
+def urlPoke(url):
+    ide = re.findall('\d+', url)
+    id = ide[1]
+    return id
 
 def getPokemon():
     url = 'https://pokeapi.co/api/v2/pokemon/'
@@ -6,6 +12,13 @@ def getPokemon():
     if response.status_code == 200:
         payload = response.json()
         results = payload.get('results',[])
+        if results:
+            for pokemon in results:
+                name = pokemon['name']
+                url = pokemon['url']
+                urls = urlPoke(url)
+                pokemon['id'] = urls
+
     return results
 
 def getInfo(id):
