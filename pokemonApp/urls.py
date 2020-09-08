6ee66be02d73bcaf9f0d45189e5d7ae4a72ce61d
Raul Gonzalez Cruz <raul.gonzalez@rotamundos.com>
2020-09-08 17:50:45 -0500

    pass id to function

--- pokemonApp/urls.py
+++ pokemonApp/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('',views.index, name ='index'),
     #path('index',views.index , name = 'index'),
     path('pokemonView',views.pokemonView, name ='pokemonView'),
-    path('pokemonType',views.pokemonType,name='pokemonType')
+    path('pokemonType',views.pokemonType,name='pokemonType'),
+    path('pokemon_Type/<int:id>',views.pokemon_Type,name='pokemon_Type'),
+    path('pokemon_View/<int:id>',views.pokemon_View, name ='pokemon_View'),
 ]
--- pokemonApp/views.py
+++ pokemonApp/views.py
@@ -2,29 +2,61 @@ from django.shortcuts import render
 from .rest import *
 
 # class PokemonAppView():
-def index(request):
+def index(request):#si no es get return negacion 
     if request.method == 'GET':
         pokemons= getPokemon()
         context = {
             'pokemones':pokemons
         }
         return render(request,'pokemonApp/index.html',context)
+    else:
+        return False 
         
 def pokemonView(request):
-    pokemon_id = request.GET['id']
-    types,abilities = getInfo(pokemon_id)    
-    context = {
-            'types':types,
-            'abilities':abilities
+    if request.method == 'GET':
+        pokemon_id = request.GET['id']
+        types,abilities = getInfo(pokemon_id)    
+        context = {
+                'types':types,
+                'abilities':abilities
 
         }      
-    return render(request,'pokemonApp/pokemonView.html',context)
+        return render(request,'pokemonApp/pokemonView.html',context)
+    else:
+        return False 
 
 def pokemonType(request):
-    type_id = request.GET['id']
-    pokemonts = getType(type_id)
-    context = {
-        'pokemonts':pokemonts
-    }
+    if request.method == 'GET':
+        type_id = request.GET['id']
+        pokemonts = getType(type_id)                                                      
+        context = {
+            'pokemonts':pokemonts
+        }
 
-    return render(request,'pokemonApp/pokemonType.html',context)
+        return render(request,'pokemonApp/pokemonType.html',context)
+    else:
+        return False 
+
+def pokemon_Type(request,id):
+    if request.method == 'GET':
+        pokemonts = getType(id)                                                      
+        context = {
+            'pokemonts':pokemonts
+        }
+
+        return render(request,'pokemonApp/pokemonType.html',context)
+    else:
+        return False 
+
+        
+def pokemon_View(request,id):
+    if request.method == 'GET':
+        types,abilities = getInfo(id)    
+        context = {
+                'types':types,
+                'abilities':abilities
+
+        }      
+        return render(request,'pokemonApp/pokemonView.html',context)
+    else:
+        return False 
